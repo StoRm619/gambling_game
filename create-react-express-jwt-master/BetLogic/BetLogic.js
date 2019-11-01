d4a78f97ecc68d8c83d6b628e24506c569928cc9
BlackAce <alfredo.gon195@gmail.com>
2019-10-31 19:51:58 -0700

    game logic take pool and calc winings

---
+++ create-react-express-jwt-master/BetLogic/BetLogic.js
@@ -0,0 +1,22 @@
+
+let chronos = user.money
+
+let TeamA = 0;
+let TeamB =0;
+let pool = TeamA + TeamB;
+
+winner = teamA || teamB
+for (var i = 0; i < teamA.length; i++) {
+     TeamA += parseInt(teamA[i])
+}
+
+for (var i = 0; i < teamB.length; i++) {
+    TeamB += parseInt(teamB[i])
+}
+
+if (winner === user[i].team){
+    pool * (user/winner) = earned
+    user[i].money += earned
+} else{
+    
+}
