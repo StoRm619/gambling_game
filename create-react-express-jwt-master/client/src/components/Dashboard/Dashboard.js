686279ff911a72947bc7932a148d30178bfb6f7a
BlackAce <alfredo.gon195@gmail.com>
2019-11-05 20:59:59 -0800

    blank chat

--- create-react-express-jwt-master/client/src/components/Dashboard/Dashboard.js
+++ create-react-express-jwt-master/client/src/components/Dashboard/Dashboard.js
@@ -2,13 +2,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
+/* import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import ListItemText from "@material-ui/core/ListItemText"; */
 import Chip from "@material-ui/core/Chip";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import { CTX } from "./Store";
+import { CTX } from "../Store/Store.js";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -44,18 +44,18 @@ export default function Dashboard() {
   const { allChats, sendChatAction, user } = React.useContext(CTX);
   const topics = Object.keys(allChats);
   // local state
-  const [activeTopic, changeActiveTopic] = React.useState(topics[0]);
+  const [activeTopic, /* changeActiveTopic */] = React.useState(topics[0]);
   const [textValue, changeTextValue] = React.useState("");
 
   return (
     <div>
       <Paper className={classes.root}>
-        <Typography variant="h4" component="h3">
+        {/* <Typography variant="h4" component="h3">
           Chat app.
         </Typography>
-        <Typography component="h5">{activeTopic}</Typography>
+        <Typography component="h5">{activeTopic}</Typography> */}
         <div className={classes.flex}>
-          <div className={classes.topicsWindow}>
+          {/* <div className={classes.topicsWindow}>
             <List>
               {topics.map(topic => (
                 <ListItem
@@ -67,7 +67,7 @@ export default function Dashboard() {
                 </ListItem>
               ))}
             </List>
-          </div>
+          </div> */}
           <div className={classes.chatWindow}>
             {allChats[activeTopic].map((chat, i) => (
               <div className={classes.flex} key={i} >
