5ddb7cb6e74ce627ee753c740da5184bcbf6baea
Christin Boner <christinmclemons@gmail.com>
2019-11-07 10:54:51 -0800

    prepping for death merge

--- create-react-express-jwt-master/client/src/components/ChosenStream/ChosenStream.js
+++ create-react-express-jwt-master/client/src/components/ChosenStream/ChosenStream.js
@@ -21,55 +21,73 @@ class ChosenStream extends Component {
                 teamALogo: res.data[2].opponents[0].opponent.image_url,
                 teamBLogo: res.data[2].opponents[1].opponent.image_url,
                 gameNumber: res.data[2].number_of_games,
-                scheduledAt: res.data[2].scheduled_at
+                scheduledAt: res.data[2].scheduled_at,
+                liveLink: res.data[2].live_url
             })
         }
         )
     }
     render () {
     return (
-        <div className="chosenTwitchPlayer">
-            <iframe
-                src="https://player.twitch.tv/?channel=summit1g&muted=true&autoplay=false"
-                height="100%"
-                width="100%"
-                frameBorder="0"
-                title="mainTwitch"
-                scrolling="no"
-                >
-            </iframe>
-            <div className="streamDetails">
-                <div className="detailBetContainer">
-                    <Container>
-                        <h6>Event Name</h6>
-                        <Form>
-                            <FormGroup>
-                                <Row>
-                                    <Col>
-                                        <CustomInput type="radio" name="teamChoice" id="teamABetChoice" label={this.state.teamAName} inline />
-                                    </Col>
-                                    <Col>
-                                        <CustomInput type="radio" name="teamChoice" id="teamBBetChoice" label={this.state.teamBName} inline />
-                                    </Col>
-                                    <Col>
-                                        <Label for="betAmount">Bet Total:</Label>
-                                        <Input type="select" name="betAmount" id="betAmountSelect" inline>
-                                            <option>$10.00</option>
-                                            <option>$15.00</option>
-                                            <option>$20.00</option>
-                                            <option>$25.00</option>
-                                            <option>$50.00</option>
-                                        </Input>
-                                    </Col>
-                                </Row>
-                            </FormGroup>
-                            <Button className="submit">Submit</Button>
-                        </Form>
-                    </Container>
-                </div>
-            </div>
+      <div className="chosenTwitchPlayer">
+        <iframe
+          src={this.state.liveLink ? this.state.liveLink : "https://player.twitch.tv/?channel=summit1g&muted=true&autoplay=false"}
+          height="100%"
+          width="100%"
+          frameBorder="0"
+          title="mainTwitch"
+          scrolling="no"
+        ></iframe>
+        <div className="streamDetails">
+          <div className="detailBetContainer">
+            <Container>
+              <h6>Event Name</h6>
+              <Form>
+                <FormGroup>
+                  <Row>
+                    <Col>
+                      <Label for="betAmount">Bet Total:</Label>
+                      <CustomInput
+                        type="radio"
+                        name="teamChoice"
+                        id="teamABetChoice"
+                        label={this.state.teamAName}
+                        inline
+                      />
+                    </Col>
+                    <Col>
+                      <CustomInput
+                        type="radio"
+                        name="teamChoice"
+                        id="teamBBetChoice"
+                        label={this.state.teamBName}
+                        inline
+                      />
+                    </Col>
+                    <Col>
+                      <Label for="betAmount">Bet Total:</Label>
+                      <Input
+                        type="select"
+                        name="betAmount"
+                        id="betAmountSelect"
+                        inline
+                      >
+                        <option>$10.00</option>
+                        <option>$15.00</option>
+                        <option>$20.00</option>
+                        <option>$25.00</option>
+                        <option>$50.00</option>
+                      </Input>
+                    </Col>
+                  </Row>
+                </FormGroup>
+                <Button className="submit">Submit</Button>
+              </Form>
+            </Container>
+          </div>
         </div>
-    )
+      </div>
+    );
     }
 }
 
